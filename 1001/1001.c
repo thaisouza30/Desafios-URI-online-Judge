bc21a21176c875837903acadfe686d29a85f8593
Thais <39959377+thaisouza30@users.noreply.github.com>
2020-05-30 14:20:55 -0300

    Resolução 1001

---
+++ 1001/1001.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+
+int main(int argc, char *argv[]) {
+	int a,b,x; 
+   	scanf("%d",&a);
+   	scanf("%d",&b);
+   	x = a + b;
+   	printf(" X = %d \n",x);
+
+	return 0;
+}
