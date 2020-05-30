c0f4ebaae5de20d0e93729c8ce90622775297077
Thais <39959377+thaisouza30@users.noreply.github.com>
2020-05-30 20:52:16 -0300

    1006

---
+++ 1006.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+ 
+int main() {
+ 
+   	double A,B,C,MEDIA;
+   
+   	scanf("%lf",&A);
+   	scanf("%lf",&B);
+   	scanf("%lf",&C);
+   	MEDIA = (A*2 + B*3 + C*5) / 10.0;
+   
+   	printf("MEDIA = %.1lf\n", MEDIA);
+ 
+	return 0;
+}
