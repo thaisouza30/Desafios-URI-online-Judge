641f41f03bb30ed5fef49f6e47c78e25b7379d21
Thais <39959377+thaisouza30@users.noreply.github.com>
2020-07-11 18:04:38 -0300

    1013

---
+++ 1013.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+	
+	int A, B, C, MaiorAB;
+	scanf("%d",&A);
+	scanf("%d",&B); 
+	scanf("%d",&C); 
+	
+	MaiorAB = (A + B + abs (A - B)) / 2;
+	MaiorAB = (C + MaiorAB + abs (MaiorAB - C)) / 2;
+	
+	printf("%d eh o maior\n",MaiorAB);
+
+	return 0;
+}
